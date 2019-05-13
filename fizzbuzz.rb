d84ada34d999deb82a68ebab74022e82a95b3738
Robert Ramirez <robertmr8251@gmail.com>
2019-05-13 02:20:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,19 +1,5 @@
 def 
   fizzbuzz (int)
-  if int % 3 == 0 
-    "fizz"
-  end
-end
-
-def
-  fizzbuzz (int)
-  if int % 5 == 0 
-    "buzz"
-  end
-end
-
-def 
-  fizzbuzz (int)
   if int % 3 == 0 && % 5 == 0
     "fizzbuzz"
   elsif int % 5 == 0
