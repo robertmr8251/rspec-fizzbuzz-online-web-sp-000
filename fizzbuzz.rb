8ce84e01899fed5eeb5ec6c4df2e5776f099496a
Robert Ramirez <robertmr8251@gmail.com>
2019-05-13 02:26:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def 
   fizzbuzz (int)
   if int % 3 == 0 && int % 5 == 0
-    puts "Fizzbuzz"
+     "Fizzbuzz"
   elsif int % 5 == 0
-     puts "Buzz"
+     "Buzz"
      elsif int % 3 == 0 
-      puts "Fizz"
+     "Fizz"
    else
   end
 end
